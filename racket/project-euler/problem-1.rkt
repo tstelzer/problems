8d5ab927c0a5441b97664b058acde496de61f31d
Timm Stelzer <timmstelzer@gmail.com>
2018-05-02 20:53:31 +0200

    racket: Rewrite euler/1 with foldl.

--- racket/project-euler/problem-1.rkt
+++ racket/project-euler/problem-1.rkt
@@ -1,14 +1,12 @@
 #lang racket
 
-(require srfi/1)
-
 ; Problem 1
 ; If we list all the natural numbers below 10 that are multiples of 3 or 5, 
 ; we get 3, 5, 6 and 9. The sum of these multiples is 23.
 ; Find the sum of all the multiples of 3 or 5 below 1000.
 
 (define (sumWhen pred? xs)
-  (reduce (λ (n sum) (if (pred? n) (+ n sum) sum)) 0 xs))
+  (foldl (λ (n sum) (if (pred? n) (+ n sum) sum)) 0 xs))
 
 (sumWhen (λ (n)
             (or (zero? (modulo n 3))
